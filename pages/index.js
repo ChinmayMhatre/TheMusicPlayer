c4c42bdfe7d9c9d4fe3e047ecb95e35ad5ba46c5
Chinmay Mhatre <chinmaymhatre12@gmail.com>
2023-01-10 15:20:59 +0530

    login basic

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
+import { signOut, useSession } from 'next-auth/react'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const {data:session,status} = useSession();
+  console.log(session);
   return (
     <div className={styles.container}>
       <Head>
@@ -14,6 +17,14 @@ export default function Home() {
       <main >
         <h1 className="text-4xl font-bold text-center">
           Welcome to <a href="https://nextjs.org">Next.js</a>
+          <button 
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => {
+              signOut()
+            }}
+          >
+              logout
+          </button>
         </h1>
       </main>
 
--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { getProviders, signIn } from "next-auth/react";
 
-const Login = () => {
+const Login = ({ providers }) => {
    return (
-      <div>Login</div>
-   )
-}
+      <div className="flex flex-col items-center bg-black justify-center min-h-screen text-center">
+         <h1 className="text-4xl text-white font-bold">Login</h1>
+         <div className="flex flex-col items-center justify-center mt-10">
+            {Object.values(providers).map((provider) => (
+               <div key={provider.name}>
+                  <button
+                     className="px-4 py-2 mt-4 text-white bg-[#18d860] font-bold rounded-md"
+                     onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+                  >
+                     Sign in with {provider.name}
+                  </button>
+               </div>
+            ))}
+         </div>
+      </div>
+   );
+};
 
-export default Login
+export default Login;
+
+export async function getServerSideProps() {
+   const providers = await getProviders();
+   return {
+      props: {
+         providers,
+      },
+   };
+}
